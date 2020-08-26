adf80257343a44a77427a6c8134836c3205b7ab2
Robert Jakobs <robert.jakobs@sap.com>
2020-08-26 17:02:05 +0200

    Fix lint issues

--- core/xml/file_writer.py
+++ core/xml/file_writer.py
@@ -1,31 +1,29 @@
 from pathlib import Path
 import time
-from xml.etree.ElementTree import ElementTree
 
 from core.xml.utils import get_pretty_xml
 
 
-def write_xml(tree, filename=None, path="../../output/", ):
-    if(filename is None):
+def write_xml(tree, filename=None, path="output/", ):
+    if filename is None:
         filename = "scenario_output_" + time.strftime("%Y%m%d-%H%M%S") + ".xml"
-    if(not filename.endswith(".xml")):
+    if not filename.endswith(".xml"):
         filename += ".xml"
-    if(not path.endswith("/")):
+    if not path.endswith("/"):
         path += "/"
-    if(path.startswith("/")):
+    if path.startswith("/"):
         path = path[1:]
-    current_dir = Path(__file__).parent
-    joined_path = (current_dir / path).resolve()
-    createOutputFolder(joined_path)
-    write_tree(tree, joined_path, filename)
+
+    create_output_folder(path)
+    write_tree(tree, path, filename)
 
 
 def write_tree(tree, path, filename):
-    path_to_file = (path / filename).resolve()
-    prettyXML = get_pretty_xml(tree)
+    path_to_file = path + filename
+    pretty_xml = get_pretty_xml(tree)
     with open(path_to_file, "w") as f:
-        f.write(prettyXML)
+        f.write(pretty_xml)
 
 
-def createOutputFolder(path):
+def create_output_folder(path):
     Path(path).mkdir(parents=True, exist_ok=True)
