feecabe86d7d5bd018bb6bca8d7dc258c22277da
Robert Jakobs <robert.jakobs@sap.com>
2020-08-27 15:01:22 +0200

    Add config validation step for duplicate class names

--- core/configuration/validator.py
+++ core/configuration/validator.py
@@ -1,5 +1,6 @@
 from core.plugin.loader import get_plugin_classes, get_module_class_names
-from core.configuration.utils import get_pipeline_step_names
+from core.configuration.utils import get_pipeline_step_names, get_duplicates
+
 
 def check_valid_config(config):
     # checks the configuration file for missing entities
@@ -14,8 +15,16 @@ def check_pipeline_config_valid(pipeline_config):
     # check if all configured pipeline steps actually exist via a class in the plugins section
     classes = get_plugin_classes()
     class_names = get_module_class_names(classes)
+    check_duplicate_class_names(class_names)
     pipeline_step_names = get_pipeline_step_names(pipeline_config)
     is_subset = set(pipeline_step_names).issubset(class_names)
     if not is_subset:
         raise Exception(
             "One or more of the configured pipeline steps do not correspond to a valid class name")
+
+
+def check_duplicate_class_names(class_names):
+    duplicates = get_duplicates(class_names)
+    if duplicates:
+        raise Exception(
+            f'Only globally unique class names are allowed. Found duplicates {duplicates}')
