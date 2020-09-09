32deb4a205822db7198eb7b162651d072a0a5318
Robert Jakobs <robert.jakobs@sap.com>
2020-09-09 21:26:58 +0200

    Add blacklist feature for Scenario plugin

--- plugins/scenario.py
+++ plugins/scenario.py
@@ -1,5 +1,6 @@
 from xml.etree.ElementTree import Element
 
+from core.helpers.utils import extend_scenarios
 
 class Scenario:
     def __init__(self, carla_client, config, data_provider, step_idx):
@@ -8,6 +9,18 @@ class Scenario:
             raise Exception("Scenario generator property 'type' is mandatory")
         if "name_prefix" not in config:
             config["name_prefix"] = config["type"]
+        if "map_blacklist" in config and not isinstance(config["map_blacklist"], list):
+            raise Exception("Scenario generator property 'map_blacklist' needs to be a list")
+        if "map_blacklist" in config and not config["map_blacklist"]:
+            raise Exception("Scenario generator property 'map_blacklist' is not allowed to be empty")
+        all_maps = data_provider.get_available_maps_simple_name()
+        if "map_blacklist" in config:
+            is_subset = set(config["map_blacklist"]).issubset(all_maps)
+            if not is_subset:
+                raise Exception(f'Scenario generator property "map_blacklist" has one value which is not in {all_maps}')
+        if "map_blacklist" not in config:
+            config["map_blacklist"] = []
+        self.maps = [carla_map for carla_map in all_maps if carla_map not in config["map_blacklist"]]
         self.config = config
         self.data_provider = data_provider
         self.step_idx = step_idx
@@ -15,8 +28,7 @@ class Scenario:
     def generate(self, tree):
         root = tree.getroot()
         scenarios = []
-        maps = self.data_provider.get_available_maps_simple_name()
-        for idx, town in enumerate(maps):
+        for idx, town in enumerate(self.maps):
             scenario = Element("scenario")
             scenario.set("name", f'{self.config["name_prefix"]}{idx}-{self.step_idx}')
             scenario.set("type", self.config["type"])
