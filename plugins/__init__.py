c17ff0e263f403c4412a44cd6ac3b671cab510e8
Robert Jakobs <robert.jakobs@sap.com>
2020-09-18 14:50:43 +0200

    Add plugins to docs

---
+++ plugins/__init__.py
@@ -0,0 +1,13 @@
+"""
+Plugins
+=======
+.. automodule:: plugins.actor
+   :members:
+   :undoc-members:
+.. automodule:: plugins.scenario
+   :members:
+   :undoc-members:
+.. automodule:: plugins.weather
+   :members:
+   :undoc-members:
+"""
