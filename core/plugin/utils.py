49f49707e89e7c923b99662d3f6bd15bf835cd63
Robert Jakobs <robert.jakobs@sap.com>
2020-09-18 15:25:01 +0200

    Add check for implemented functions and its args for plugins

--- core/plugin/utils.py
+++ core/plugin/utils.py
@@ -9,3 +9,14 @@ def get_module_class_names(classes):
     :rtype: list
     """
     return [c.__name__ for c in classes]
+
+
+def get_implemented_class_functions(_class):
+    """Returns a list of implemented functions of a class (with internal methods starting with '__')
+
+    :param _class: Class from where to get the methods
+    :type _class: object
+    :returns: Names of methods implemented in the passed class
+    :rtype: list
+    """
+    return [func for func in dir(_class) if callable(getattr(_class, func))]
--- core/plugin/validator.py
+++ core/plugin/validator.py
@@ -1,4 +1,6 @@
-from core.plugin.utils import get_module_class_names
+import inspect
+
+from core.plugin.utils import get_module_class_names, get_implemented_class_functions
 from core.helpers.utils import get_duplicates
 
 
@@ -10,6 +12,8 @@ def check_plugins(classes):
     """
     class_names = get_module_class_names(classes)
     check_duplicate_class_names(class_names)
+    for _class in classes:
+        check_implemented_functions(_class)
 
 
 def check_duplicate_class_names(class_names):
@@ -22,3 +26,24 @@ def check_duplicate_class_names(class_names):
     if duplicates:
         raise Exception(
             f'Only globally unique class names are allowed. Found duplicates {duplicates}')
+
+
+def check_implemented_functions(_class):
+    """Raises an exception if the passed class has not implemented the mandatory methods
+
+    :param _class: Class to check
+    :type _class: object
+    """
+    mandatory_functions_to_implement = [('generate', 2), ('__init__', 5)]
+    implemented_class_function_names = get_implemented_class_functions(_class)
+    for function in mandatory_functions_to_implement:
+        function_name = function[0]
+        number_function_mandatory_params = function[1]
+        # check if the method is implemented in the class
+        if function_name not in implemented_class_function_names:
+            raise Exception(f"Method {function_name} not implemented in class {_class.__name__}")
+        ref_function = getattr(_class, function_name)
+        # check if the method is expecting the mandatory number of arguments
+        if not len(inspect.getfullargspec(ref_function).args) == number_function_mandatory_params:
+            raise Exception(
+                f"Method {function_name} implemented in class {_class.__name__} is not expecting {number_function_mandatory_params} passed arguments")
