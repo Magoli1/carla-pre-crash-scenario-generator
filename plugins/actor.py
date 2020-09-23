3db86597b6e7eecf87ddd61df5380024cf2742c0
Nicolas Reuter <nicolas.reuter98@gmail.com>
2020-09-23 15:03:43 +0000

    Actors: Read junction_id and start_point_way from xml

--- plugins/actor.py
+++ plugins/actor.py
@@ -1,5 +1,3 @@
-import pprint
-
 from xml.etree.ElementTree import SubElement
 
 from core.helpers.utils import extend_scenarios
@@ -38,23 +36,39 @@ class Actor:
             config["positioning"]["junctions"]["left"] = True
         if "right" not in config["positioning"]["junctions"]:
             config["positioning"]["junctions"]["right"] = True
-        if "streets" not in config["positioning"]:
+
+        if config["tag"] != "other_actor" and "streets" not in config["positioning"]:
             config["positioning"]["streets"] = True
-        if (not config["positioning"]["streets"]
-                and all(switch is False for switch in config["positioning"]["junctions"].values())):
-            raise Exception(
-                "Actor generators optional properties 'streets' "
-                "and all directions in 'junctions' cannot both be 'False'"
-            )
+
         if "relative_to_ego" not in config["positioning"]["junctions"]:
             config["positioning"]["junctions"]["relative_to_ego"] \
-                = {"straight": False, "left": False, "right": False}
+                = {"straight": True, "left": True, "right": True}
         if "straight" not in config["positioning"]["junctions"]["relative_to_ego"]:
             config["positioning"]["junctions"]["relative_to_ego"]["straight"] = True
         if "left" not in config["positioning"]["junctions"]["relative_to_ego"]:
             config["positioning"]["junctions"]["relative_to_ego"]["left"] = True
         if "right" not in config["positioning"]["junctions"]["relative_to_ego"]:
             config["positioning"]["junctions"]["relative_to_ego"]["right"] = True
+
+        if (config["tag"] == "other_actor"
+                and all(switch is False for switch in config["positioning"]["junctions"]["relative_to_ego"].values())):
+            raise Exception(
+                "Actor generators optional properties in "
+                "'relative_to_ego' cannot all be 'False' for 'other_actor'"
+            )
+        if config["tag"] == "other_actor" and config["positioning"].get("streets") is True:
+            raise Exception(
+                "Actor generators optional property 'streets' "
+                "cannot be 'True' for 'other_actor'"
+            )
+        if (not config["positioning"]["streets"]
+                and all(switch is False for switch in config["positioning"]["junctions"].values()
+                        if isinstance(switch, bool))):
+            raise Exception(
+                "Actor generators optional properties 'streets' "
+                "and all directions in 'junctions' cannot both be 'False'"
+            )
+
         self.config = config
         self.data_provider = data_provider
         self.step_idx = step_idx
@@ -65,8 +79,22 @@ class Actor:
             actor = SubElement(scenario, self.config["tag"])
             attributes = {}
             if self.config["tag"] == "other_actor":
-                allowed_waypoints = self.get_allowed_waypoints_in_town(scenario.get("town"))
-                # print(scenario.find("ego_vehicle").items())
+                relative_to_ego = not all(switch is False for switch in
+                                          self.config["positioning"]["junctions"]["relative_to_ego"].values())
+                ego_vehicle = scenario.find("ego_vehicle")
+                junction_id = None
+                start_point_yaw = None
+                if ego_vehicle is not None:
+                    junction_id = ego_vehicle.get("junction_id")
+                    start_point_yaw = ego_vehicle.get("start_point_yaw")
+                if junction_id is not None:
+                    junction_id = int(junction_id)
+                if start_point_yaw is not None:
+                    start_point_yaw = float(start_point_yaw)
+                allowed_waypoints = self.get_allowed_waypoints_in_town(scenario.get("town"),
+                                                                       relative_to_ego,
+                                                                       junction_id,
+                                                                       start_point_yaw)
             else:
                 allowed_waypoints = self.get_allowed_waypoints_in_town(scenario.get("town"))
             if not allowed_waypoints:
@@ -81,22 +109,29 @@ class Actor:
             attributes["actor_model"] = random.choice(self.actor_models)
             self.decorate_actor(actor, **attributes)
 
-    def get_allowed_waypoints_in_town(self, town_name):
+    def get_allowed_waypoints_in_town(self, town_name: str, relative_to_ego: bool = False,
+                                      junction_id: int = None, start_point_yaw: float = None):
         pos_config = self.config["positioning"]
         waypoints_in_town = self.data_provider.get_waypoints_per_map()[town_name]
         allowed_waypoints = []
-        if pos_config["junctions"]["straight"]:
-            allowed_waypoints += [(*waypoints, junction["object"].id) for junction in waypoints_in_town["junctions"].values()
-                                  for waypoints in junction["waypoints_with_straight_turn"]]
-        if pos_config["junctions"]["left"]:
-            allowed_waypoints += [(*waypoints, junction["object"].id) for junction in waypoints_in_town["junctions"].values()
-                                  for waypoints in junction["waypoints_with_left_turn"]]
-        if pos_config["junctions"]["right"]:
-            allowed_waypoints += [(*waypoints, junction["object"].id) for junction in waypoints_in_town["junctions"].values()
-                                  for waypoints in junction["waypoints_with_right_turn"]]
-        if pos_config["streets"]:
-            allowed_waypoints += [(waypoint, None, None, None)
-                                  for waypoint in waypoints_in_town["streets"]]
+
+        if relative_to_ego:
+            if not junction_id or not start_point_yaw:
+                return []
+
+        else:
+            if pos_config["junctions"]["straight"]:
+                allowed_waypoints += [(*waypoints, junction["object"].id) for junction in waypoints_in_town["junctions"].values()
+                                    for waypoints in junction["waypoints_with_straight_turn"]]
+            if pos_config["junctions"]["left"]:
+                allowed_waypoints += [(*waypoints, junction["object"].id) for junction in waypoints_in_town["junctions"].values()
+                                    for waypoints in junction["waypoints_with_left_turn"]]
+            if pos_config["junctions"]["right"]:
+                allowed_waypoints += [(*waypoints, junction["object"].id) for junction in waypoints_in_town["junctions"].values()
+                                    for waypoints in junction["waypoints_with_right_turn"]]
+            if pos_config["streets"]:
+                allowed_waypoints += [(waypoint, None, None, None)
+                                      for waypoint in waypoints_in_town["streets"]]
 
         return allowed_waypoints
 
