611db0a0ae2dacd6bb2b5c753f0dc791ec3f8ae5
Robert Jakobs <robert.jakobs@sap.com>
2020-09-27 15:31:31 +0200

    Add custom logger

--- core/logger/log_formatter.py
+++ core/logger/log_formatter.py
@@ -1,25 +1,38 @@
 import logging
 
+# This is needed for windows and will filter all color escape sequences from the formatter
+# and replaces them with the ones from windows
+from colorama import init
+
+init()
+
 
 class CustomFormatter(logging.Formatter):
     """Logging Formatter to add colors and count warning / errors"""
 
+    green = "\x1b[0;32m"
     grey = "\x1b[38;21m"
     yellow = "\x1b[33;21m"
     red = "\x1b[31;21m"
     bold_red = "\x1b[31;1m"
     reset = "\x1b[0m"
-    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+    format = "%(asctime)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
 
     FORMATS = {
         logging.DEBUG: grey + format + reset,
-        logging.INFO: grey + format + reset,
+        logging.INFO: green + format + reset,
         logging.WARNING: yellow + format + reset,
         logging.ERROR: red + format + reset,
         logging.CRITICAL: bold_red + format + reset
     }
 
     def format(self, record):
+        """Formats logs with configured colors
+
+        :param record: Record to format
+        :type record: object
+
+        """
         log_fmt = self.FORMATS.get(record.levelno)
         formatter = logging.Formatter(log_fmt)
         return formatter.format(record)
