aa7d2a5461ce337d0ac31544f2d0898704a293cf
Robert Jakobs <robert.jakobs@sap.com>
2020-09-30 22:51:10 +0200

    Add performance test files

--- generator.py
+++ generator.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 import carla
 import argparse
+import csv
 import logging
+import time
+import os
+import psutil
+import copy
+
 
 from core.xml.initializer import initialize_xml_tree
 from core.xml.file_writer import write_xml
@@ -38,12 +44,24 @@ def start_pipeline(carla_client, generator_config, data_provider, log_level):
     :type data_provider: object
     """
     pipelines = generator_config["pipelines"]
+    new_pipelines = []
+    item = pipelines[0]["Test"]
+    for number in range(1000):
+        newItem = {f"Test{number}": copy.deepcopy(item)}
+        new_pipelines.append(newItem)
+    pipelines = new_pipelines
+    description = ["Before Dataprovider run", "After Dataprovider run"]
+    process = psutil.Process(os.getpid())
+    times = [time.time()]
+    ram = [process.memory_info()[0]]
     if generator_config["dataprovider"]["preload"]:
         logger.info("### START preloading data START ###")
         data_provider.preload()
         logger.info("### END preloading data END ###")
     logger.info("### Running pipelines... ###")
-    for pipeline in pipelines:
+    times.append(time.time())
+    ram.append(process.memory_info()[0])
+    for number_pipeline, pipeline in enumerate(pipelines):
         pipeline_name = extract_pipeline_name(pipeline)
         logger.info(f'## START pipeline "{pipeline_name}" START ##')
         tree = initialize_xml_tree()
@@ -62,6 +80,24 @@ def start_pipeline(carla_client, generator_config, data_provider, log_level):
             logger.info(f'# END pipeline step #{idx} {Class.__name__} END #')
         write_xml(tree, filename=pipeline_name)
         logger.info(f'## END pipeline "{pipeline_name}" END ##')
+        times.append(time.time())
+        description.append(f"Pipeline #{number_pipeline}")
+        ram.append(process.memory_info()[0]/1024)
+
+    times_with_descriptions = [description, times]
+
+    f = open('timings.csv', 'w')
+    with f:
+        writer = csv.writer(f)
+        for row in times_with_descriptions:
+            writer.writerow(row)
+
+    ram_with_description = [description, ram]
+    f = open('ram_usage.csv', 'w')
+    with f:
+        writer = csv.writer(f)
+        for row in ram_with_description:
+            writer.writerow(row)
 
 
 def get_args():
